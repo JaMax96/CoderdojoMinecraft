627aae838c1f194f5026dd50d52704dd42bc2801
Matthias Ableidinger <ableimat@gmx.at>
2020-04-15 23:51:42 +0200

    can create a region and set greetings

--- src/main/java/coderdojo/JoinAndLeaveListener.java
+++ src/main/java/coderdojo/JoinAndLeaveListener.java
@@ -1,5 +1,13 @@
 package coderdojo;
 
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldedit.math.BlockVector3;
+import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.protection.flags.Flags;
+import com.sk89q.worldguard.protection.managers.RegionManager;
+import com.sk89q.worldguard.protection.regions.ProtectedCuboidRegion;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import com.sk89q.worldguard.protection.regions.RegionContainer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,5 +18,15 @@ public class JoinAndLeaveListener implements Listener {
     public void onJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
         player.sendMessage("Dear " + player.getName() + ", welcome to our server!");
+
+        RegionContainer container = WorldGuard.getInstance().getPlatform().getRegionContainer();
+        RegionManager regions = container.get(BukkitAdapter.adapt(player.getWorld()));
+        regions.addRegion(createRegion());
+    }
+
+    private ProtectedRegion createRegion() {
+        ProtectedCuboidRegion region = new ProtectedCuboidRegion("globalid", true, BlockVector3.at(-10, 0, -10), BlockVector3.at(10, 10, 10));
+        region.setFlag(Flags.GREET_MESSAGE, "Hi there!");
+        return region;
     }
 }
