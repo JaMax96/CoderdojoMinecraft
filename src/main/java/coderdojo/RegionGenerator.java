484625cf13eb59f780d3faf809caac3ccca90b1a
Matthias Ableidinger <ableimat@gmx.at>
2020-05-01 14:02:44 +0200

    5 in a row

--- src/main/java/coderdojo/RegionGenerator.java
+++ src/main/java/coderdojo/RegionGenerator.java
@@ -4,9 +4,9 @@ import com.sk89q.worldedit.math.BlockVector3;
 
 public class RegionGenerator {
 
-    private static final int START_X = -70;
+    private static final int START_X = -110;
     private static final int START_Z = -450;
-    private static final int END_X = 70;
+    private static final int END_X = 110;
     private static final int END_Z = 450;
     private static final int PLOT_SIZE = 30;
     private static final int PLOT_DISTANCE = 8;
